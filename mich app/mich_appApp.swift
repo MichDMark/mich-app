80fdbc98b00c10bfc400cca992a97e8905054a41
Mich DMark <michdmark@gmail.com>
2022-10-06 20:18:03 -0500

    Initial Commit

---
+++ mich app/mich_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  mich_appApp.swift
+//  mich app
+//
+//  Created by Mich DMark on 06/10/22.
+//
+
+import SwiftUI
+
+@main
+struct mich_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
