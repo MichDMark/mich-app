50d0972a589bdd6d2195649969e4d0c257b6eecf
Mich DMark <michdmark@gmail.com>
2022-10-14 21:50:55 -0500

    imagenes y botones en swiftUI

---
+++ mich app/Images.swift
@@ -0,0 +1,49 @@
+//
+//  Images.swift
+//  mich app
+//
+//  Created by Mich DMark on 12/10/22.
+//
+
+import SwiftUI
+
+struct Images: View {
+    var body: some View {
+        VStack
+        {
+            Text("imagenes")
+            
+            Image("unboxing")
+                .resizable()
+                .aspectRatio(contentMode: .fit).frame(width:500, height: 150, alignment: .center)
+            
+            Image("unboxing")
+                .resizable()
+                .aspectRatio(contentMode: .fit).frame(width:500, height: 150, alignment: .center)
+                .blur(radius: 3)
+            
+            Image("unboxing")
+                .resizable()
+                .aspectRatio(contentMode: .fit).frame(width:500, height: 150, alignment: .center)
+                .opacity(0.5)
+            
+            Button {
+                print("pulsa video")
+            }
+                label: {
+                Image("unboxing")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit).frame(width:500, height: 150, alignment: .center)
+            }
+            
+            Image(systemName: "moon.fill")
+
+        }
+    }
+}
+
+struct Images_Previews: PreviewProvider {
+    static var previews: some View {
+        Images()
+    }
+}
